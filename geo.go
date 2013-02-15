ec21ad7def53b3a343058bdf4229ae494f23fe30
Kelly <defaultstring@gmail.com>
2013-02-14 21:51:09 -0800

    [src] Adding in comments, introducing a new point function\!

--- geo.go
+++ geo.go
@@ -4,12 +4,45 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/bmizerany/pq"
+	"math"
 )
 
 // Represents a Physical Point in geographic notation [lat, lng]
 type Point struct {
 	lat float64
 	lng float64
+}
+
+// Original Implementation from: http://www.movable-type.co.uk/scripts/latlong.html
+// @param [float64] dist.  The arc distance in which to transpose the origin point (in meters).
+// @param [float64] bearing.  The compass bearing in which to transpose the origin point (in degrees).
+// @return [*Point].  Returns a Point struct populated with the lat and lng coordinates
+//                    of transposing the origin point a certain arc distance at a certain bearing.
+func (p *Point) PointAtDistanceAndBearing(dist float64, bearing float64) *Point {
+	// Earth's radius ~= 6,356.7523km
+	// TODO Constantize
+	dr := dist / 6356.7523
+
+	bearing = (bearing * (math.Pi / 180.0))
+
+	lat1 := (p.lat * (math.Pi / 180.0))
+	lng1 := (p.lng * (math.Pi / 180.0))
+
+	lat2_part1 := math.Sin(lat1) * math.Cos(dr)
+	lat2_part2 := math.Cos(lat1) * math.Sin(dr) * math.Cos(bearing)
+
+	lat2 := math.Asin(lat2_part1 + lat2_part2)
+
+	lng2_part1 := math.Sin(bearing) * math.Sin(dr) * math.Cos(lat1)
+	lng2_part2 := math.Cos(dr) - (math.Sin(lat1) * math.Sin(lat2))
+
+	lng2 := lng1 + math.Atan2(lng2_part1, lng2_part2)
+	lng2 = math.Mod((lng2+3*math.Pi), (2*math.Pi)) - math.Pi
+
+	lat2 = lat2 * (180.0 / math.Pi)
+	lng2 = lng2 * (180.0 / math.Pi)
+
+	return &Point{lat: lat2, lng: lng2}
 }
 
 // The Mapper interface
@@ -25,6 +58,8 @@ type SQLConf struct {
 	lngCol  string
 }
 
+// A Mapper that uses Standard SQL Syntax 
+// to perform intersting geo-related mapping functions and queries
 type SQLMapper struct {
 	conf    *SQLConf
 	sqlConn *sql.DB
@@ -32,7 +67,8 @@ type SQLMapper struct {
 
 var DefaultSQLConf = &SQLConf{driver: "postgres", openStr: "user=postgres password=*** dbname=points sslmode=disable", table: "points", latCol: "lat", lngCol: "lng"}
 
-// geo.HandleWithSQL
+// @return [*SQLMapper]. An instantiated SQLMapper struct with the DefaultSQLConf.
+// @return [Error]. Any error that might have occured during instantiating the SQLMapper.  
 func HandleWithSQL() (*SQLMapper, error) {
 	s := &SQLMapper{conf: DefaultSQLConf}
 
@@ -46,14 +82,14 @@ func HandleWithSQL() (*SQLMapper, error) {
 	return s, err
 }
 
+// Original implemenation from : http://www.movable-type.co.uk/scripts/latlong-db.html
 func (s *SQLMapper) Within(p *Point, radius float32) (*sql.Rows, error) {
-	// Taken from : http://www.movable-type.co.uk/scripts/latlong-db.html
 	select_str := fmt.Sprintf("SELECT * FROM %s a", s.conf.table)
 	lat1 := fmt.Sprintf("sin(radians(%f)) * sin(radians(a.lat))", p.lat)
 	lng1 := fmt.Sprintf("cos(radians(%f)) * cos(radians(a.lat)) * cos(radians(a.lng) - radians(%f))", p.lat, p.lng)
 	where_str := fmt.Sprintf("WHERE acos(%s + %s) * %f <= %f", lat1, lng1, 6356.7523, radius)
 	query := fmt.Sprintf("%s %s", select_str, where_str)
-	
+
 	res, err := s.sqlConn.Query(query)
 	if err != nil {
 		panic(err)
@@ -63,6 +99,7 @@ func (s *SQLMapper) Within(p *Point, radius float32) (*sql.Rows, error) {
 }
 
 /*
+TODO Incoporate into README
 func main() {
 	s, err := HandleWithSQL()
 	if err != nil {
--- geo_test.go
+++ geo_test.go
@@ -2,40 +2,11 @@ package geo
 
 import (
 	"fmt"
-	"math"
 	"testing"
 )
 
-func FlushTestDB(s * SQLMapper) {
+func FlushTestDB(s *SQLMapper) {
 	s.sqlConn.Exec("DELETE FROM points;")
-}
-
-func calculateTargetLocation(lat float64, lng float64, dist float64, bearing float64) (float64, float64) {
-	// Earth's radius ~= 6,356.7523km
-        dr := dist / 6356.7523
-
-	bearing = (bearing * (math.Pi / 180.0))
-
-	lat1 := (lat * (math.Pi / 180.0))
-	lng1 := (lng * (math.Pi / 180.0))
-
-	lat2_part1 := math.Sin(lat1) * math.Cos(dr)
-	lat2_part2 := math.Cos(lat1) * math.Sin(dr) * math.Cos(bearing)
-
-	lat2 := math.Asin(lat2_part1 + lat2_part2)
-
-	lng2_part1 := math.Sin(bearing) * math.Sin(dr) * math.Cos(lat1)
-	lng2_part2 := math.Cos(dr) - (math.Sin(lat1) * math.Sin(lat2))
-
-	lng2 := lng1 + math.Atan2(lng2_part1, lng2_part2)
-
-	// What why is this here?
-	lng2 = math.Mod((lng2 + 3*math.Pi), (2 * math.Pi)) - math.Pi
-
-	lat2 = lat2 * (180.0 / math.Pi)
-	lng2 = lng2 * (180.0 / math.Pi)
-
-	return lat2, lng2
 }
 
 // @spec:
@@ -46,23 +17,15 @@ func TestWithin(t *testing.T) {
 	// SFO
 	origin := &Point{37.619002, -122.37484}
 
-	in_points := make([]*Point, 360)
-	out_points := make([]*Point, 360)
-
 	for i := 0; i < 360; i++ {
 		bearing := (float64)(i * 1.0)
 
-		in_lat, in_lng := calculateTargetLocation(origin.lat, origin.lng, 7.9, bearing)
-		s.sqlConn.Exec(fmt.Sprintf("INSERT INTO points(lat, lng) VALUES(%f, %f);", in_lat, in_lng))
+		in_point := origin.PointAtDistanceAndBearing(7.9, bearing)
+		s.sqlConn.Exec(fmt.Sprintf("INSERT INTO points(lat, lng) VALUES(%f, %f);", in_point.lat, in_point.lng))
 
-		out_lat, out_lng := calculateTargetLocation(origin.lat, origin.lng, 8.1, bearing)
-		s.sqlConn.Exec(fmt.Sprintf("INSERT INTO points(lat, lng) VALUES(%f, %f);", in_lat, in_lng))
+		out_point := origin.PointAtDistanceAndBearing(8.1, bearing)
+		s.sqlConn.Exec(fmt.Sprintf("INSERT INTO points(lat, lng) VALUES(%f, %f);", out_point.lat, out_point.lng))
 
-		in_point := &Point{lat: in_lat, lng: in_lng}
-		in_points[i] = in_point
-
-		out_point := &Point{lat: out_lat, lng: out_lng}
-		out_points[i] = out_point
 	}
 
 	res, err := s.Within(origin, 8)
