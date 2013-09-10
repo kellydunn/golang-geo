61d2620b51fe51629faf6e550a0b68ded3213a25
kellydunn <defaultstring@gmail.com>
2013-09-09 18:14:38 -0700

    [src] using my branch of libpq temporarily

--- geo.go
+++ geo.go
@@ -2,7 +2,7 @@ package geo
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
+	_ "github.com/kellydunn/pq"
 )
 
 // @return [*SQLMapper]. An instantiated SQLMapper struct with the DefaultSQLConf.
