41a1ed2f25083ca38e0c30b67a819fb5c2c97bd2
kellydunn <defaultstring@gmail.com>
2013-09-16 18:56:19 -0700

    [src] Adding some documentation to the geocoder.go interface

--- geocoder.go
+++ geocoder.go
@@ -1,5 +1,8 @@
 package geo
 
+// This interface describes a Geocoder, which provides the ability to Geocode and Reverse Geocode geographic points of interest.
+// Geocoding should accept a string that represents a street address, and returns a pointer to a Point that most closely identifies it.
+// Reverse geocoding should accept a pointer to a Point, and return the street address that most closely represents it.
 type Geocoder interface {
 	Geocode(query string) (*Point, error)
 	ReverseGeocode(p *Point) (string, error)
