dcfeee72fbd9c4c7489c7513b50ac34d8e3c7ac0
kellydunn <defaultstring@gmail.com>
2014-01-19 15:19:02 -0800

    Moving yaml parsing logic into seperate method such that we can provide a seperate code path for getting configuration from any file the user wishes

--- sql_conf.go
+++ sql_conf.go
@@ -38,69 +38,77 @@ func sqlConfFromEnv() *SQLConf {
 }
 
 // Attempts to read config/geo.yml, and creates a SQLConf as described therein.
-// Returns the DefaultSQLConf if no config/geo.yml is found, or an error if one arises during the process of parsing the configuration file.
+// Returns the DefaultSQLConf if no config/geo.yml is found, or an error
+// if one arises during the process of parsing the configuration file.
 func GetSQLConf() (*SQLConf, error) {
 	DefaultSQLConf := sqlConfFromEnv()
 
 	// TODO This should be redesigned so that the user specifies where the config file is
-	//      We can still handle the issue where it doesn't exist, but that way it's not hardcoded.
+	//      We can still handle the issue where it doesn't exist,
+	//      but that way it's not hardcoded.
 	configPath := path.Join("config/geo.yml")
 	_, err := os.Stat(configPath)
 	if err != nil && os.IsNotExist(err) {
 		return DefaultSQLConf, nil
 	} else {
 
-		// Defaults to development environment, you can override by changing the $GO_ENV variable:
-		// `$ export GO_ENV=environment` (where environment can be "production", "test", "staging", etc.
-		// TODO Potentially find a better solution to handling environments
-		// https://github.com/adeven/goenv ?
+		// Defaults to development environment, you can override
+		// by changing the $GO_ENV variable:
+		// `$ export GO_ENV=environment` (where environment
+		// can be "production", "test", "staging", etc.)
+		// TODO: Potentially find a better solution to handling environments
+		// Perhaps: https://github.com/adeven/goenv ?
 		goEnv := os.Getenv("GO_ENV")
 		if goEnv == "" {
 			goEnv = "development"
 		}
 
 		config, readYamlErr := yaml.ReadFile(configPath)
-		if readYamlErr == nil {
-
-			// TODO Refactor this into a more generic method of retrieving info
-
-			// Get driver
-			driver, driveError := config.Get(fmt.Sprintf("%s.driver", goEnv))
-			if driveError != nil {
-				return nil, driveError
-			}
-
-			// Get openStr
-			openStr, openStrError := config.Get(fmt.Sprintf("%s.openStr", goEnv))
-			if openStrError != nil {
-				return nil, openStrError
-			}
-
-			// Get table
-			table, tableError := config.Get(fmt.Sprintf("%s.table", goEnv))
-			if tableError != nil {
-				return nil, tableError
-			}
-
-			// Get latCol
-			latCol, latColError := config.Get(fmt.Sprintf("%s.latCol", goEnv))
-			if latColError != nil {
-				return nil, latColError
-			}
-
-			// Get lngCol
-			lngCol, lngColError := config.Get(fmt.Sprintf("%s.lngCol", goEnv))
-			if lngColError != nil {
-				return nil, lngColError
-			}
-
-			sqlConf := &SQLConf{driver: driver, openStr: openStr, table: table, latCol: latCol, lngCol: lngCol}
-			return sqlConf, nil
-
+		if readYamlErr != nil {
+			return nil, readYamlErr
 		}
 
-		return nil, readYamlErr
+		return confFromYamlFile(config, goEnv)
 	}
 
 	return nil, err
+}
+
+func confFromYamlFile(config *yaml.File, goEnv string) (*SQLConf, error) {
+
+	// TODO Refactor this into a more generic method of retrieving info
+
+	// Get driver
+	driver, driveError := config.Get(fmt.Sprintf("%s.driver", goEnv))
+	if driveError != nil {
+		return nil, driveError
+	}
+
+	// Get openStr
+	openStr, openStrError := config.Get(fmt.Sprintf("%s.openStr", goEnv))
+	if openStrError != nil {
+		return nil, openStrError
+	}
+
+	// Get table
+	table, tableError := config.Get(fmt.Sprintf("%s.table", goEnv))
+	if tableError != nil {
+		return nil, tableError
+	}
+
+	// Get latCol
+	latCol, latColError := config.Get(fmt.Sprintf("%s.latCol", goEnv))
+	if latColError != nil {
+		return nil, latColError
+	}
+
+	// Get lngCol
+	lngCol, lngColError := config.Get(fmt.Sprintf("%s.lngCol", goEnv))
+	if lngColError != nil {
+		return nil, lngColError
+	}
+
+	sqlConf := &SQLConf{driver: driver, openStr: openStr, table: table, latCol: latCol, lngCol: lngCol}
+	return sqlConf, nil
+
 }
