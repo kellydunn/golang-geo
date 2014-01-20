8672101b98d812fa1126a5636862a8dab7be25bb
kellydunn <defaultstring@gmail.com>
2014-01-19 16:51:35 -0800

    Adding additional tests to assert the behvaior of SQL Mappers

--- sql_mapper_test.go
+++ sql_mapper_test.go
@@ -7,7 +7,8 @@ import (
 	"testing"
 )
 
-// Ensures that
+// Ensures that creating a new SQL Mapper does not encounter an error upon initialization
+// And also matches the expected database connections and sql configurations.
 func TestNewSQLMapper(t *testing.T) {
 	conf := sqlConfFromEnv()
 	db, _ := sql.Open(conf.driver, conf.openStr)
@@ -15,11 +16,27 @@ func TestNewSQLMapper(t *testing.T) {
 	env := os.Getenv("DB")
 	filepath := fmt.Sprintf("db/%s/dbconf.yml", env)
 	s, _ := NewSQLMapper(filepath, db)
+
 	if s == nil {
 		t.Error("Expected NewSqlMapper to return a non-nil pointer to a sql mapper")
 	}
 
 	if s.sqlConn != db {
 		t.Error()
+	}
+}
+
+// Ensures that creating a new SQLMapper and getting its SQL DB Connection
+// is the same *sql.DB used in initialization.
+func TestSqlDbConn(t * testing.T) {
+	conf := sqlConfFromEnv()
+	db, _ := sql.Open(conf.driver, conf.openStr)
+
+	env := os.Getenv("DB")
+	filepath := fmt.Sprintf("db/%s/dbconf.yml", env)
+	s, _ := NewSQLMapper(filepath, db)
+
+	if s.SqlDbConn() != db {
+		t.Error("Expected db connections are mismatched.")
 	}
 }
