273430b7e0fbefaad0e06e3fbf6470d85aa88764
kellydunn <defaultstring@gmail.com>
2014-03-17 21:49:56 -0700

    Adding more active comments to the method signatures of the polygon methods

--- polygon.go
+++ polygon.go
@@ -4,27 +4,31 @@
 
 package geo
 
-// Polygon is carved out of a 2D plane by a set of (possibly disjoint) contours.
+// A Polygon is carved out of a 2D plane by a set of (possibly disjoint) contours.
 // It can thus contain holes, and can be self-intersecting.
 type Polygon struct {
 	Contours []*Contour
 }
 
-// Add is a convenience method for appending a contour to a polygon.
+// Appends the passed in contour to the current Polygon.
 func (p *Polygon) Add(c *Contour) {
 	p.Contours = append(p.Contours, c)
 }
 
+// Returns whether or not the current Polygon contains the passed in Point.
 // For geoJSON polygons, the first polygon is the outer polygon, all secondary
 // polygons are internal cut outs. e.g. the centre of a donut.
-func (poly Polygon) Contains(p *Point) bool {
-	for i, c := range poly.Contours {
-		if i == 0 && !c.Contains(p) {
+func (p *Polygon) Contains(point *Point) bool {
+	for i, c := range p.Contours {
+
+		if i == 0 && !c.Contains(point) {
 			return false
 		}
-		if i != 0 && c.Contains(p) {
+
+		if i != 0 && c.Contains(point) {
 			return false
 		}
 	}
+
 	return true
 }
