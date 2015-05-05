3bd15f599d7d0772e42046f3fad21df724f6ac3b
kellydunn <defaultstring@gmail.com>
2015-05-04 22:36:34 -0700

    Adding in new tests for mapquest geocoder

---
+++ mapquest_geocoder_test.go
@@ -0,0 +1,29 @@
+package geo
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestMapQuestExtractLatLngFromRequest(t *testing.T) {
+	data, err := GetMockResponse("test/data/mapquest_geocode_success.json")
+	if err != nil {
+		t.Error("%v\n", err)
+	}
+
+	res := []*mapQuestGeocodeResponse{}
+
+	err = json.Unmarshal(data, &res)
+	if err != nil {
+		t.Error("%v\n", err)
+	}
+
+	if len(res) <= 0 {
+		t.Error("Unexecpected amount of results for mapquest mock response")
+	}
+
+	if res[0].Lat != 37.62181845 || res[0].Lng != -122.383992092462 {
+		t.Error(fmt.Sprintf("Expected: [37.62181845, -122.383992092462], Got: [%f, %f]", res[0].Lat, res[0].Lng))
+	}
+}
