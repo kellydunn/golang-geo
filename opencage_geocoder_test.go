285ffe3fe49eb37b4a26e153ced9bc9792cc3d20
kellydunn <defaultstring@gmail.com>
2015-05-10 11:26:52 -0700

    Adding in opencage geocoder testing coverage for utility functions

--- opencage_geocoder_test.go
+++ opencage_geocoder_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
+func TestSetOpencageAPIKey(t * testing.T) {
+	SetOpenCageAPIKey("foo")
+	if OpenCageAPIKey != "foo" {
+		t.Errorf("Mismatched value for OpencageAPIKey.  Expected: 'foo', Actual: %s", OpenCageAPIKey)
+	}
+}
+
+func TestSetOpenCageGeocodeURL(t *testing.T) {
+	SetOpenCageGeocodeURL("foo")
+	if opencageGeocodeURL != "foo" {
+		t.Errorf("Mismatched value for googleGeocoeURL.  Expected: 'foo', Actual: %s", opencageGeocodeURL)
+	}
+}
+
 func TestUnmarshalOpenCageGeocoderResponse(t *testing.T) {
 	// TODO implement
 }
